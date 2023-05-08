87d828eeae9984296e71a44ff1830d6b233373ca
Edwina Messenger <edwinamessenger@icloud.com>
2023-05-08 22:44:45 +1000

    move btn in front of logo

--- js/index.js
+++ js/index.js
@@ -3,5 +3,5 @@ const followSection = document.getElementById('follow');
 const body = document.querySelector('body')
 
 followBtn.addEventListener('click', () => {
-    followSection.scrollIntoView({behavior: 'smooth'})
+    followSection.scrollIntoView({behavior: 'smooth'})  
 })
